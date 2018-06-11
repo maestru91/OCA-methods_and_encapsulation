c1683639c7a8cd648e62bdfc9885876afb817065
Cepoi-Scarletia, Sorin X (UK - Overseas) <sorincepoiscarletia@deloitte.co.uk>
2018-06-11 20:31:48 +0300

    lambda stuff

---
+++ src/test/java/lambdaExpressions/lambdaExpression.java
@@ -0,0 +1,50 @@
+package lambdaExpressions;
+
+
+public class lambdaExpression {
+
+    public static void main(String args[]) {
+        lambdaExpression tester = new lambdaExpression();
+
+        //with type declaration
+        MathOperation addition = (int a, int b) -> a + b;
+
+        //with out type declaration
+        MathOperation subtraction = (a, b) -> a - b;
+
+        //with return statement along with curly braces
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        //without return statement and without curly braces
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        //without parenthesis
+        GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        //with parenthesis
+        GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+        greetService1.sayMessage("Sorin");
+        greetService2.sayMessage("Ionut");
+    }
+
+    // This are predicates (interface with only one method)
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    private int operate(int a, int b, MathOperation mathOperation) {
+        return mathOperation.operation(a, b);
+    }
+}
